4f3f49f5e1e24ae9551b5719e6091840fcaab4e4
Shrinivas Medewar <SHRINIVASMEDEWAR12@GMAIL.COM>
2026-03-24 08:48:44 +0530

    Update index.tsx

--- src/sections/AcheivementsSection/index.tsx
+++ src/sections/AcheivementsSection/index.tsx
@@ -9,7 +9,7 @@ export const AchievementsSection = () => {
 
                 </div>
                 <div className="box-border caret-transparent gap-x-6 grid grid-cols-none gap-y-6 md:grid-cols-[repeat(4,minmax(0px,1fr))]">
-                    <AchievementCard iconUrl="https://c.animaapp.com/mmq5nl3br7cuRw/assets/icon-22.svg" iconVariant="text-green-500" badge="50+" title="LeetCode Achiever" description="Solved 600+ problems and earned 27 badges including Daily, Monthly & Yearly badges." />
+                    <AchievementCard iconUrl="https://c.animaapp.com/mmq5nl3br7cuRw/assets/icon-22.svg" iconVariant="text-green-500" badge="50+" title="LeetCode Achiever" description="Solved 50+ problems and earned 27 badges including Daily, Monthly & Yearly badges." />
                     <AchievementCard iconUrl="https://c.animaapp.com/mmq5nl3br7cuRw/assets/icon-23.svg" iconVariant="text-blue-500" badge="Certified" title="Azure AI-900 Certified" description="Earned Microsoft AI-900 Certification via Credly." showButton={true} buttonText="View Certificate" />
                     <AchievementCard iconUrl="https://c.animaapp.com/mmq5nl3br7cuRw/assets/icon-24.svg" iconVariant="text-yellow-500" badge="Certified" title="NVIDIA DL Certified" description="Completed Deep Learning course and received official certification from NVIDIA." showButton={true} buttonText="View Certificate" />
                     <AchievementCard iconUrl="https://c.animaapp.com/mmq5nl3br7cuRw/assets/icon-25.svg" iconVariant="text-purple-500" badge="Top 6" title="Hackathon Finalist" description="Selected as a finalist among the Top 6 teams in SASI Hackathon at Tadepalligudem,AP" />
