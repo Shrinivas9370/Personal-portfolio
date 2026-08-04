f6f9a1ab007d1d2059ede0c03aef35337a285a7f
Shrinivas Medewar <SHRINIVASMEDEWAR12@GMAIL.COM>
2026-08-05 00:59:16 +0530

    Update EducationTimeline.tsx

--- src/sections/AboutSection/components/EducationTimeline.tsx
+++ src/sections/AboutSection/components/EducationTimeline.tsx
@@ -7,14 +7,14 @@ export const EducationTimeline = () => {
                     <div className="box-border caret-transparent">
                         <h4 className="text-xl font-semibold box-border caret-transparent leading-7">               B.Tech in Computer Science             </h4>
                         <p className="text-purple-400 font-medium box-border caret-transparent mt-2">               Vellore Institute of Technology             </p>
-                        <p className="text-slate-400 box-border caret-transparent mt-2">               2026 – Present | CGPA: -            </p>
+                        <p className="text-slate-400 box-border caret-transparent mt-2">               2026 – Present | CGPA: -  7.5          </p>
                     </div>
                 </div>       </div>       <div className="bg-zinc-900 border-b-slate-800 border-l-sky-500 border-r-slate-800 border-t-slate-800 shadow-[rgba(0,0,0,0)_0px_0px_0px_0px,rgba(0,0,0,0)_0px_0px_0px_0px,rgba(0,0,0,0.05)_0px_1px_2px_0px] box-border caret-transparent mt-6 rounded-xl border-l-4 border-b border-r border-t border-solid">
                 <div className="box-border caret-transparent p-6">
                     <div className="box-border caret-transparent">
                         <h4 className="text-xl font-semibold box-border caret-transparent leading-7">               Senior Secondary (Class XII)             </h4>
                         <p className="text-purple-400 font-medium box-border caret-transparent mt-2">               Tier 2 College             </p>
-                        <p className="text-slate-400 box-border caret-transparent mt-2">               2025 | 89.7%             </p>
+                        <p className="text-slate-400 box-border caret-transparent mt-2">               2025 | 71.4%             </p>
                     </div>
                 </div>
             </div>
